9cc68e8d8ede0233367416b4e4e312395b1b70e4
dhilipan182005 <praveen161navy@gmail.com>
2026-04-02 14:34:06 +0530

    commit change

---
+++ Long Programming/Order/src/main/java/com/example/order/Entity/Product.java
@@ -0,0 +1,24 @@
+package com.example.order.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private double price;
+    private int quantity;
+}
---
+++ Long Programming/Order/src/main/java/com/example/order/Repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.order.Repository;
+
+import com.example.order.Entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByName(String name);
+    List<Product> findByPriceLessThan(double price);
+}
